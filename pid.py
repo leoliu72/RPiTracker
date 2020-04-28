1e3ea802b143770100c5cb79d86c84262ff2f66c
leoliu72 <liu_jiaming@berkeley.edu>
2020-04-28 12:39:39 -0700

    Cleaned up PID class

--- pid.py
+++ pid.py
@@ -5,35 +5,38 @@ class PID():
         self.kP = kP
         self.kI = kI
         self.kD = kD
-        
+
     def initialize(self):
         # initialize current and previous time
         self.currTime = time.time()
         self.prevTime = self.currTime
-        
+
         # initialize PID summation terms
         self.prevError = 0
         self.error = 0
         self.errorInt = 0
         self.errorDer = 0
-        
+
+    # Updates the PID loop
     def update(self, error, sleep = 0.2):
         # Sleep for a bit
         time.sleep(sleep)
-        
-        # Find deltaTime and set new prevTime 
+
+        # Find change in time
         self.currTime = time.time()
         dt = self.currTime - self.prevTime
-        self.prevTime = self.currTime
-        
+
         # Set proportional term to error input
         self.error = error
         # Set integral term to keep adding. Implement antiwindup later
-        self.errorInt += error * dTime
+        self.errorInt += error * dt
         # Set derivative term
-        self.errorDer = (self.error - self.prevError) / dt
+        self.errorDer = (error - self.prevError) / dt
+
+        # Set prev terms for next loop
         self.prevError = self.error
-        
+        self.prevTime = self.currTime
+
         servoAngle = self.kP * self.error + self.kI * self.errorInt + self.kD * self.errorDer
-        
+
         return servoAngle
